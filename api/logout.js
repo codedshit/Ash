f33d81152d0d2dd9a60fc7101b5aab60e88dc66b
Ashy <asdfghjklash857@gmail.com>
2026-07-28 10:29:50 +0300

    Delete api directory

--- api/logout.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (req, res) => {
-    res.setHeader('Set-Cookie', `session=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0`);
-    res.status(200).json({ ok: true });
-};
